ecb212c55b1d8ae28029cc5fe7b960c442d492d6
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-16 20:20:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+print directors_database.length
 pp directors_database
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
