966e576725f5a696df48d5c6ccf6a5bc2ebc2b52
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-16 20:23:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 print directors_database.length
+# director = [
+#{name: "Eduardo", movies: ["1","2","3"]},
+#{name: "Eduardo", movies: ["1","2","3"]}, 
+#{name: "Eduardo", movies: ["1","2","3"]}
+]
 pp directors_database
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
