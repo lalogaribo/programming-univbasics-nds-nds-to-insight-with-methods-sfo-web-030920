a38546d259e1f4bd248074b1d0c531854c01f4da
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-16 20:43:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ print directors_database.length
 #{name: "Eduardo", movies: ["1","2","3"]},
 #{name: "Eduardo", movies: ["1","2","3"]}, 
 #{name: "Eduardo", movies: ["1","2","3"]}
-]
+#]
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
